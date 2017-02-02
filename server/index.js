1ac152ad8659bd1f8bb782ee95f8c232eadace17
ericajlittle <ericalittle123@gmail.com>
2017-02-02 22:34:14 +0000

    reformatted directory

--- server/index.js
+++ server/index.js
@@ -5,13 +5,16 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession( {
   name: 'session',
   keys: ['secret'],
   maxAge: 24 * 60 * 60 * 1000
 }));
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
 
 app.get('/register', (request, response) => {
   response.redirect('/register');
@@ -24,56 +27,6 @@ app.get('/checkout', (request, response) => {
 app.get('/status', (request, response) => {
   response.redirect('/status');
 });
-
-$("create-order").on("submit", (event) => {
-    event.preventDefault();
-// creates order id row and adds order-id to cookie
-  });
-
-  $("menu").on("submit", (event) => {
-    event.preventDefault();
-    // if item doesnt exist
-    $.ajax({
-      method: "POST",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-    // if item does exist
-    $.ajax({
-      method: "PUT",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-  });
-
-  $("delete-from-cart").on("submit", (event) => {
-    event.preventDefault();
-    $.ajax({
-      method: "DELETE",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-  });
-
-// Ajax post to add items to cart
-function renderCart(items) {
-    item.forEach(items => {
-      $("#cart").prepend(createItem(item));
-    });
-  }
-
-function loadItems () {
-    $.ajax({
-      method: "GET",
-      url: "/carts"
-    }).then((respose) => {
-      $("#cart").empty();
-      renderCart(respose)
-    });
-  }
 
 
 app.listen(PORT, () => {
