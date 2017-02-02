c6924d4552f2f927d61f3bad83ae1ab4543f6e8e
Arman Kazemi <kas1er@hotmail.com>
2017-02-02 21:34:35 +0000

    I'm just trying to make sure my index.html is up to date with the master version

--- server/index.js
+++ server/index.js
@@ -25,57 +25,6 @@ app.get('/status', (request, response) => {
   response.redirect('/status');
 });
 
-$("create-order").on("submit", (event) => {
-    event.preventDefault();
-// creates order id row and adds order-id to cookie
-  });
-
-  $("menu").on("submit", (event) => {
-    event.preventDefault();
-    // if item doesnt exist
-    $.ajax({
-      method: "POST",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-    // if item does exist
-    $.ajax({
-      method: "PUT",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-  });
-
-  $("delete-from-cart").on("submit", (event) => {
-    event.preventDefault();
-    $.ajax({
-      method: "DELETE",
-      url: "/carts",
-      data: data
-    })
-    loadItems();
-  });
-
-// Ajax post to add items to cart
-function renderCart(items) {
-    item.forEach(items => {
-      $("#cart").prepend(createItem(item));
-    });
-  }
-
-function loadItems () {
-    $.ajax({
-      method: "GET",
-      url: "/carts"
-    }).then((respose) => {
-      $("#cart").empty();
-      renderCart(respose)
-    });
-  }
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
