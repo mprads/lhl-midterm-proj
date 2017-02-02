5369314b5d3b891d3529f001337dc71dce73f6d3
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-02 17:07:40 +0000

    Server runs

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession( {
+  name: 'session',
+  keys: ['secret'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+app.get('/', (request, response) => {
+  console.log('hello');
+  return;
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
