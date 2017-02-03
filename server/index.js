700a930ddac68cad8f0b7df98f6db768feaffa34
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-03 01:24:24 +0000

    Fixed package and link issues

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-app.set('view engine', 'ejs');
+const sass = require("node-sass-middleware");
 
-app.use(bodyParser.urlencoded({extended: true}));
+const knexConfig = require("../knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger  = require('knex-logger');
+
+app.use(morgan('dev'));
+app.use(knexLogger(knex));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
 app.use(cookieSession( {
   name: 'session',
   keys: ['secret'],
